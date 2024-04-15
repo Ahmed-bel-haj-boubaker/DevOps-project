a566c4f52a2f565503ce307bd25d84c2350e147b
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-04-15 07:00:17 +0100

    test

--- src/test/java/tn/esprit/rh/achat/SpringBootappTest.java
+++ src/test/java/tn/esprit/rh/achat/SpringBootappTest.java
@@ -9,10 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.rh.achat.controllers.CategorieProduitController;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
 import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,12 +30,25 @@ public class SpringBootappTest {
     private CategorieProduitServiceImpl categorieProduitService;
 
     @Mock
-    private CategorieProduitRepository categorieProduitRepository;
+    private CategorieProduitController categorieProduitController;
 
     @Test
     public void getAllTest(){
-        Mockito.when(categorieProduitRepository.findAll()).thenReturn(Stream.of(new CategorieProduit(22,"123A","pull")).collect(Collectors.toList()));
-        assertEquals(2,categorieProduitService.retrieveAllCategorieProduits().size());
+        CategorieProduit categorie1 = new CategorieProduit(1, "Category 1", "Description 1");
+        CategorieProduit categorie2 = new CategorieProduit(2, "Category 2", "Description 2");
+        List<CategorieProduit> expectedList = Arrays.asList(categorie1, categorie2);
+
+        when(categorieProduitService.retrieveAllCategorieProduits()).thenReturn(expectedList);
+
+        // Act
+        List<CategorieProduit> actualList = categorieProduitController.getCategorieProduit();
+
+        // Assert
+        assertEquals(expectedList.size(), actualList.size(), "Lists should have the same size");
+        for (int i = 0; i < expectedList.size(); i++) {
+            assertEquals(expectedList.get(i), actualList.get(i), "Category at index " + i + " should match");
+        }
+
     }
 
 }
